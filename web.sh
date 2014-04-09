20a56eb3eff5a992a572fd019c07afee4f2e4f85
alex <Alexander Brigham>
2014-04-09 08:53:27 -0400

    Reduced console output

--- web.sh
+++ web.sh
@@ -6,33 +6,32 @@ export DEBIAN_FRONTEND
 
 # Get software listings and update
 echo "Updating..."
-apt-get update -y > /dev/null
-apt-get dist-upgrade -y > /dev/null
-echo "Done"
+apt-get update -y > dev/null 2>&1
+apt-get dist-upgrade -y > dev/null 2>&1
+echo "...Done"
 
 # Install required software
-echo "Installing Node.js..."
+echo "Installing Node.js...\n\tInstalling dependencies..."
 apt-get install -y python-software-properties 	# Add "apt-add-repository"
 add-apt-repository ppa:chris-lea/node.js 		# Install node from latest versions
-apt-get update -y > /dev/null
+echo "\t...Done"
+apt-get update -y > dev/null 2>&1
 apt-get install -y nodejs
-echo "Done"
+echo "...Done"
 
 echo "Installing NPM modules..."
 npm config set registry http://registry.npmjs.org/	# Fix problem with NPM + vagrant setup (https://github.com/npm/npm/issues/2119)
 oldDir=$(pwd)
 cd /vagrant
-npm install > /dev/null
+npm install  > dev/null 2>&1
 cd $oldDir
-echo "Done"
+echo "...Done"
 
 # Install utility software
 echo "Installing utility software..."
-apt-get install -y vim screen
-echo "Done"
+apt-get install -y vim screen  > dev/null 2>&1
+echo "...Done"
 
 # Reinstall virtualbox guest additions (needed if kernel update occured)
-echo "Updating VirtualBox Guest Additions..."
 apt-get install -y make linux-headers-$(uname -r)	# Required for vbox setup
 /etc/init.d/vboxadd setup
-echo "Done"
