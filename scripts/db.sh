eb03105bdb132ff00386b894a020af3c22ff9d82
alex <Alexander Brigham>
2014-04-13 22:19:29 -0400

    Streamlined MongoDB server provisioning

--- scripts/db.sh
+++ scripts/db.sh
@@ -4,10 +4,9 @@
 DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 
-# Get software list and update
-echo "Updating..."
+# Get software list
+echo "Updating software listing..."
 apt-get update -y  > /dev/null
-apt-get dist-upgrade -y  > /dev/null 2>&1
 echo "...Done"
 
 
@@ -19,11 +18,4 @@ echo "...Done"
 # Install utility software
 echo "Installing utility software..."
 apt-get install -y vim screen  > /dev/null 2>&1
-echo "...Done"
-
-# Reinstall virtualbox guest additions (needed if kernel update occured)
-echo "Installing VirtualBox Guest Additions..."
-apt-get install -y make
-apt-get install -y linux-headers-$(uname -r) # Required for vbox setup
-/etc/init.d/vboxadd setup
 echo "...Done"
